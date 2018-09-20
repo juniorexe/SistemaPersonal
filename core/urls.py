e848f16df8713917d75320553a6f7d6781c96234
Eziquiel Jr <nasc.junior@gmail.com>
2018-09-19 22:48:52 -0300

    adicionado login falta fazer funcionar

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home, cadastro
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name="home"),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
     path('cadastro/', cadastro, name="cadastro"),
 ]
